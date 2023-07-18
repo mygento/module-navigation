24db7431729a9d8b0275e34b6090e06164411b2a
nikita tarasov <nikita@mygento.ru>
2023-07-18 15:27:34 +0300

    init

---
+++ .php-cs-fixer.php
@@ -0,0 +1,14 @@
+<?php
+$header = <<<EOF
+@author Mygento Team
+@copyright 2023 Mygento (https://www.mygento.com)
+@package Mygento_Navigation
+EOF;
+
+$finder = PhpCsFixer\Finder::create()
+    ->in('.')
+    ->ignoreVCSIgnored(true);
+
+$config = new \Mygento\CS\Config\Module($header);
+$config->setFinder($finder);
+return $config;
---
+++ registration.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * @author Mygento Team
+ * @copyright 2023 Mygento (https://www.mygento.com)
+ * @package Mygento_Navigation
+ */
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Mygento_Navigation',
+    __DIR__
+);
